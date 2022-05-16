d19a96a4c93774801ae2e2ed33bf47ef562d9d8e
JesusDiaz2003 <produran420@gmail.com>
2022-05-16 14:36:25 -0500

    tarea cifrado cesar

---
+++ tareas/TareaCifrado/src/tareacifrado/TareaCifrado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tareacifrado;
+
+import java.text.Normalizer;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HP
+ */
+public class TareaCifrado {
+   String input = "Hola mundo";
+        String v[] = {"", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    for 
+    
+}
+   
---
+++ tareas/TareaEncriptacion/src/tareaencriptacion/GraficoMain.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tareaencriptacion;
+
+/**
+ *
+ * @author HP
+ */
+public class GraficoMain {
+
+    public static void main(String[] args) {
+        ventana vs= new ventana();
+        
+    }
+}
+
